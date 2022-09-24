849f0c5493c26f6f8bbda9eb4207d09c2d7ec019
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-24 20:32:17 +0000

    init project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styles from './style';
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
