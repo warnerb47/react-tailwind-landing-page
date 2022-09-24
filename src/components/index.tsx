c70b8a6517dcb7d3df8188ce0534560eeeb8043b
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-24 20:49:17 +0000

    feat: add component structure

---
+++ src/components/index.tsx
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+
+export const components = {
+    Billing,
+    Business,
+    Button,
+    CTA,
+    CardDeal,
+    Client,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonial,
+}
