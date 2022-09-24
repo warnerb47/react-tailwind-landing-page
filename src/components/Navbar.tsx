7476ad5e6f488869e0aefb9abb93d2ec1b83afd1
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-24 22:33:06 +0000

    feat: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import { useState } from 'react'
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+  
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hookband' className='w-[124px] h-[32px]' />
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((navLink, index) => (
+          <li
+            key={navLink.id}
+            className={`font-poppins
+              font-normat
+              cursor-pointer
+              text-[16px]
+              text-white
+              ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}`} >
+            <a href={`#${navLink.id}`}>{navLink.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close: menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(prev => !prev)}
+          />
+      </div>
+
+      <div
+        className={`
+          ${toggle ? 'flex' : 'hidden'}
+          p-6 bg-black-gradient
+          absolute top-20 right-0
+          mx-4 my-2 min-w-[140px] rounded-xl sidebar`} >
+        <ul className='list-none flex flex-col justify-end items-center flex-1'>
+          {navLinks.map((navLink, index) => (
+            <li
+              key={navLink.id}
+              className={`font-poppins
+                font-normat cursor-pointer
+                text-[16px] text-white mb-4`} >
+              <a href={`#${navLink.id}`}>{navLink.title}</a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
+    </nav>
   )
 }
 
