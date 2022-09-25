0af9e99ac517e786099ae3d052a8a434761ec7ad
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-25 02:09:51 +0000

    feat: add testimonial section

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { feedback } from "../constants"
+import styles from "../style"
+import FeedbackCard from "./FeedbackCard"
 
-const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+const Testimonial = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient " />
+    <div className="w-full flex justify-between items-center md:flex-row sm:mb-16 mb-6 relative z-[1]">
+      <h1 className={styles.heading2}>What people are <br className="sm:block hidden"/> saying about us</h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iusto aliquid voluptate impedit, vitae, consequuntur omnis ex recusandae facere nisi, odio placeat architecto iste commodi expedita quae dolorem error atque?</p>
+      </div>
+    </div>
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card)=>(
+        <FeedbackCard key={card.id} {...card}/>
+      ))}
+    </div>
+  </section>
+)
 
 export default Testimonial
