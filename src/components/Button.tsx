cb1b33086ac97eb389f69b59c8adb23b3c2182f7
warnerb47 <taphagaye2001.mg@gmail.com>
2022-09-25 01:41:25 +0000

    feat: add card deal section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}: {styles: string}) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary rounded-[10px] outline-none ${styles}`}>
       Get started
     </button>
   )
--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { card } from "../assets"
+import styles, {layout} from "../style"
+import Button from "./Button"
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={`${styles.heading2}`}>Find a better card deal <br className="sm:block hidden"/> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident aperiam commodi similique tempore maxime adipisci error sequi beatae cumque culpa a laudantium reprehenderit placeat eius numquam minus, totam quo fugit.</p>
+    <Button styles="mt-10"/>
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+    </div>
+  </section>
+)
 
 export default CardDeal
